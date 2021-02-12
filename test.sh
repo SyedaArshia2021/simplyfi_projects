109349d903b1bda7eed2c4360cbc01dbc00b15fc
SyedaArshia2021 <77437494+SyedaArshia2021@users.noreply.github.com>
2021-02-12 11:45:45 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [curl -k "http://13.57.214.107:8082" | grep -i 'Congratulations']
+if [/usr/bin/curl -k "http://13.57.214.107:8082" | grep -i 'Congratulations']
 then 
   exit 0
 else 
