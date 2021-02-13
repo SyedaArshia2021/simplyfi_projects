066cb9660f7e170fa9d57b752fb13d5941ee86d0
SyedaArshia2021 <77437494+SyedaArshia2021@users.noreply.github.com>
2021-02-13 19:44:56 +0530

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,21 +1,14 @@
 import { getByText } from '@testing-library/dom'
-
 import '@testing-library/jest-dom/extend-expect'
-
 import { JSDOM } from 'jsdom'
 
-import fs from 'fs'
-
-import path from 'path'
-
-
-const html = fs.readFileSync(path.resolve('./index.html'), 'utf8');
+const fs = require('fs');
+const path = require('path');
+const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
 
 
 let dom
-
 let container
-
 
 
 describe('index.html', () => {
