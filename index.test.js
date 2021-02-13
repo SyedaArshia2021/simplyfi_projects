085d200b7b6e9ade994918c7100e79a4025c0abc
SyedaArshia2021 <77437494+SyedaArshia2021@users.noreply.github.com>
2021-02-13 19:28:34 +0530

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -9,7 +9,7 @@ import fs from 'fs'
 import path from 'path'
 
 
-const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
+const html = fs.readFileSync(path.resolve(/var/lib/jenkins/workspace/job1, './index.html'), 'utf8');
 
 
 let dom
