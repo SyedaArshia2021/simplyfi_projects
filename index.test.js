4ec70ecd73bc3cbd0469deb3bc960b1a30ba886d
SyedaArshia2021 <77437494+SyedaArshia2021@users.noreply.github.com>
2021-02-14 19:47:43 +0530

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -2,13 +2,22 @@ import { getByText } from '@testing-library/dom'
 import '@testing-library/jest-dom/extend-expect'
 import React from 'react'
 import { screen,render } from '@testing-library/react'
-window.React = React
 test("pass functions to matchers", () => {
   const Hello = () => (
     <div>
-      <h1> Congratulations folks!! </h1>
+      Hello <span>world</span>
     </div>
   );
-  render(<Hello />);
-  expect(screen.getByText(' Congratulations folks!! ')).toBeInTheDocument();
-})
+  const { getByText } = render(<Hello />);
+
+  const getByTextWithMarkup = (text: string) => {
+    getByText((content, node) => {
+      const hasText = (node: HTMLElement) => node.textContent === text
+      const childrenDontHaveText = Array.from(node.children).every(
+        child => !hasText(child as HTMLElement)
+      )
+      return hasText(node) && childrenDontHaveText
+    })
+  }
+
+  getByTextWithMarkup('Hello world')
