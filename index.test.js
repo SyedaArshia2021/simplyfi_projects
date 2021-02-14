14fdd413b14603d907fc496a81c08b3f3159ff27
SyedaArshia2021 <77437494+SyedaArshia2021@users.noreply.github.com>
2021-02-14 20:00:31 +0530

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -10,7 +10,7 @@ test("pass functions to matchers", () => {
   );
   const { getByText } = render(<Hello />);
 
-  const getByTextWithMarkup = (text: string) => {
+  const getByTextWithMarkup = (text:string) => {
     getByText((content, node) => {
       const hasText = (node: HTMLElement) => node.textContent === text
       const childrenDontHaveText = Array.from(node.children).every(
