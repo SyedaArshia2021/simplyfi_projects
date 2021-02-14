027eb448741aaedf0fe869b15ad8dca002d21a4c
SyedaArshia2021 <77437494+SyedaArshia2021@users.noreply.github.com>
2021-02-14 18:27:55 +0530

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,30 +1,23 @@
 import { getByText } from '@testing-library/dom'
 import '@testing-library/jest-dom/extend-expect'
-import { screen } from '@testing-library/react'
-import { JSDOM } from 'jsdom'
-import fs from 'fs'
-import path from 'path'
+import { screen,render } from '@testing-library/react'
 
-const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
+test("pass functions to matchers", () => {
+  const Hello = () => (
+    <div>
+      <h1> Congratulations folks!! </h1>
+    </div>
+  );
+  const { getByText } = render(<Hello />);
 
-let dom
-let container
-
-describe('index.html', () => {
-  beforeEach(function(done) {
-    JSDOM.fromFile('/var/lib/jenkins/workspace/job1/index.html')
-    // Constructing a new JSDOM with this option is the key
-    // to getting the code in the script tag to execute.
-    // This is indeed dangerous and should only be done with trusted content.
-    // https://github.com/jsdom/jsdom#executing-scripts
-    .then((dom) => {
-      container = dom.window.document.body
+  const getByTextWithMarkup = (text: string) => {
+    getByText((content, node) => {
+      const hasText = (node: HTMLElement) => node.textContent === Congratulations folks!! 
+      const childrenDontHaveText = Array.from(node.children).every(
+        child => !hasText(child as HTMLElement)
+      )
+      return hasText(node) && childrenDontHaveText
     })
-    .then(done ,done)
-  })
+  }
 
-  it('renders a heading element', () => {
-    expect(getByText(container, ' Congratulations folks!! ')).toBeInTheDocument()
-    debug()
-  })
-})
+  getByTextWithMarkup(' Congratulation folks!! ')
