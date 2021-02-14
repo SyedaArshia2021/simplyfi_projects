887a69aed5ec593ae838d9bc479ffd01ee203e5c
SyedaArshia2021 <77437494+SyedaArshia2021@users.noreply.github.com>
2021-02-14 20:14:29 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import unittest
 class Test(unittest.TestCase):
    bs = None
    def setUpClass():
-      url = 'http://13.57.214.107:8082'
+      url = 'http://50.18.130.25:8082'
       Test.bs = BeautifulSoup(urlopen(url), 'html.parser')
    def test_titleText(self):
       pageTitle = Test.bs.find('h1').get_text()
