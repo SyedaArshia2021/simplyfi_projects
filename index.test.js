8877a4d4ce42d2714c76ff4231d4a663193e4003
SyedaArshia2021 <77437494+SyedaArshia2021@users.noreply.github.com>
2021-02-14 19:29:14 +0530

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,7 +1,8 @@
 import { getByText } from '@testing-library/dom'
 import '@testing-library/jest-dom/extend-expect'
+import React from 'react'
 import { screen,render } from '@testing-library/react'
-
+window.React = React
 test("pass functions to matchers", () => {
   const Hello = () => (
     <div>
